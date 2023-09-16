fe00f678a850aa0e7bdcd5131d8550f482668dfc
Umut-Umut <umutsanci25@gmail.com>
2023-09-16 04:20:18 +0300

    Game.tscn, GameArea eklendi

--- Game/Game.gd
+++ Game/Game.gd
@@ -32,3 +32,12 @@ func _on_SpaceShip_shot(collision_position):
 
 func _on_GUI_settings_change(new_settings):
 	meteor.new_settings(new_settings)
+
+func _on_GameArea_body_exited(body):
+	# bullet meteora carptigi zaman disable ediliyor. Disable sonucu area bunu exit olarak
+	# algiliyor. Tekrar disable etmemek icin is_disabled kontrolu gerceklestiriliyor.
+	if body is Bullet and body.is_disabled == false:
+		body.disable()
+
+func _on_GameArea_ready():
+	get_node("GameArea/CollisionShape2D").shape.extents = OS.get_screen_size()
